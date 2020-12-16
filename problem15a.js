60012d4e341e44dbf12ebae4b88b95d254558038
Tom Boutell <tom@apostrophecms.com>
2020-12-16 07:25:36 -0500

    working 15a

--- problem15a.js
+++ problem15a.js
@@ -1,18 +1,11 @@
-const data = [ 1, 3, 2 ];
+const data = [ 1, 2, 3 ];
 
-const turns = 2020;
-// const previousBuffer = new ArrayBuffer(turns * 4);
-// const previous = new Uint32Array(previousBuffer);
-// const lastBuffer = new ArrayBuffer(turns * 4);
-// const last = new Uint32Array(lastBuffer);
-const previous = [];
-const last = [];
+const previous = {};
+const last = {};
 let latest;
+console.log(last);
 
-for (let turn = 0; (turn < turns); turn++) {
-  // if (!(turn % 1000)) {
-    console.log(`${turn}: ${latest}`);
-  // }
+for (let turn = 0; (turn < 2020); turn++) {
   if (data.length) {
     latest = data.shift();
   } else if (previous[latest] !== undefined) {
@@ -22,5 +15,5 @@ for (let turn = 0; (turn < turns); turn++) {
   }
   previous[latest] = last[latest];
   last[latest] = turn;
+  console.log(latest);
 }
-console.log(latest);
